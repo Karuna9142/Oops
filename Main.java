a107f7db6014dbe5eab88404ca00a023dbad6c20
Karuna Kumari <mishrakaruna9142@gmail.com>
2024-09-10 14:18:20 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,44 +1,18 @@
-package com.oops;
+package com.Constructor;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		/*
-		Activa a1 = new Activa();
-		a1.bill("Activa", 30000);
-		a1.speed();
-		System.out.println(a1.name = "ActivaPlus");
-		System.out.println(a1.price= 230000);
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number:- ");
+		int num = sc.nextInt();
+		
+		ChildArmstrong c1 = new ChildArmstrong();
+		c1.printNum(num);
+		
 
-		ChildMethodOveriding c1 = new ChildMethodOveriding();
-		c1.print_info();
-		*/
-		
-		/*
-		Btech b1 = new Btech();
-		b1.clg_info("MCR ","MCA");
-		b1.dept_info("RWC ","BCA");
-		b1.tech_info("CTI", "Btech");
-		*/
-		/*
-		Internationa_business b1 = new Internationa_business();
-		b1.local_info();
-		
-		Local_business l1 = new Local_business();
-		l1.india_business();
-		l1.business_info();
-		*/
-		
-		SBI s1 = new SBI();
-		s1.SBI_info();
-		s1.rate_of_interest();
-		s1.RBI_loan_dept();
-		
-		
-		Swiss sw1 = new Swiss();
-		sw1.Swiss_info();
-		
-		
 	}
 
 }
